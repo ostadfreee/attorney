3db82fd2d7718e1f1ef839c8195de495be5e3996
ostadfreee <ostadfreee@yahoo.com>
2013-09-22 20:05:06 +0330

    Finnish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Attorney::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/law"
+  get "static_pages/university"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/views/static_pages/law.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "static_pages/law.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
