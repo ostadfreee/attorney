05799bbc760b534595898ca30efefe99073328ff
ostadfreee <ostadfreee@yahoo.com>
2013-10-19 17:48:44 +0330

    Email.com

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -1,12 +1,12 @@
 Pony.options = {
-  :to => 'abasshahravan@gmail.com',
+  :to => 'ostadfree@gmail.com',
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-    :user_name            => 'abasshahravan',
-    :password             => 'zarintajZEVK1970',
+    :user_name            => 'ostadfree',
+    :password             => 'freekhan1234',
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     #:domain               => "http://warm-caverns-6097.herokuapp.com" # the HELO domain provided by the client to the server
   }
