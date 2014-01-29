3d4ae7fffb902b74ed32cd489812be93977c06ee
ostadfreee <ostadfreee@yahoo.com>
2014-01-29 23:21:23 +0330

    Some minor changes

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
 module Attorney
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -20,5 +21,5 @@ module Attorney
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-  config.assets.initialize_on_precompile = true
+ 
 end
