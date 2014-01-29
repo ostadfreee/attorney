a0b92f4f5c23330e2a5e53ac490f7f5fc1fea04e
ostadfreee <ostadfreee@yahoo.com>
2014-01-29 22:50:43 +0330

    some change

--- config/application.rb
+++ config/application.rb
@@ -20,4 +20,5 @@ module Attorney
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+  config.assets.initialize_on_precompile = true
 end
